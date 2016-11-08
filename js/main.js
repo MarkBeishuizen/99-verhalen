c8cf481519f51ca444fed03b4e46f29fe9c574eb
Mark Beishuizen <m.beishuizen@outlook.com>
2016-11-09 00:48:26 +0100

    Vebeteringen gesprek
    ruimtes gevuld, filter verandert, micro interaction erbij.

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ for (var i = 0; i < articles.length; i++) {
 var search = document.querySelector("header input");
 
 function toggleSelected (event) {
-    var search = event.target;
+    //var search = event.target;
     search.classList.toggle("selected");
 }
 
@@ -22,8 +22,7 @@ search.addEventListener("focus", toggleSelected);
 search.addEventListener("focusout", toggleSelected);
 
 
-var buttons = document.querySelectorAll(".content article button");
-console.log(buttons);
+var buttons = document.querySelectorAll(".button-interaction");
 
 function toggleButtonHover (event) {
     var button = event.target;
@@ -35,4 +34,25 @@ for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("mouseenter", toggleButtonHover);
     buttons[i].addEventListener("mouseleave", toggleButtonHover);
 }
+
+var buttons = document.querySelectorAll(".download");
+
+function activateDownloadAnimation (event) {
+    var button = event.target;
+    if (button.classList.contains("clicked")) {
+        return;
+    }
+    
+    button.classList.add("clicked");
+    button.innerHTML = "Dowloaden...";
+    setTimeout(function() {
+        button.innerHTML = "Gedownload"; 
+    }, 2000);
+} 
+
+for (var i = 0; i < buttons.length; i++) {
+    console.log(buttons[i]);
+    buttons[i].addEventListener("click", activateDownloadAnimation);
+}
+
 
